3521c3704412f0f8e2039d46cc5cf99b2052b2ae
Tom Meier <tom@venombytes.com>
2014-08-28 14:24:18 +1000

    OCLint boxen package and installer

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,48 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# include oclint
+class oclint {
+  require wget
+  $version = '0.7'
+
+  file { "/opt/boxen/cache/oclint-${version}-x86_64-apple-darwin-10.tar.gz":
+    ensure => present,
+    require => Exec['Fetch oclint'],
+  }
+
+  exec { 'Fetch oclint':
+    cwd => '/opt/boxen/cache',
+    command => "wget -O oclint-${version}.tar.gz http://archives.oclint.org/releases/0.7/oclint-${version}-x86_64-apple-darwin-10.tar.gz",
+    creates => "/opt/boxen/cache/oclint-${version}.tar.gz",
+    path    => ['/opt/boxen/homebrew/bin'];
+  }
+
+  exec { 'Extract oclint':
+    cwd     => '/opt/boxen/cache',
+    command => "tar xvf /opt/boxen/cache/oclint-${version}.tar.gz",
+    creates => "/opt/boxen/cache/oclint-${version}",
+    path    => ['/usr/bin'],
+    require => Exec['Fetch oclint'];
+  }
+
+  file { "/opt/boxen/cache/oclint-${version}":
+    require => Exec['Extract oclint'];
+  }
+
+  exec { 'Extract oclint libraries':
+    cwd     => '/usr/local',
+    command => "cp -rp /opt/boxen/cache/oclint-${version}/lib/* /usr/local/lib/",
+    onlyif => [
+                "test ! -d /usr/local/lib/oclint",
+                "test ! -d /usr/local/lib/clang",
+              ],
+    path    => ['/usr/bin'],
+    require => File["/opt/boxen/cache/oclint-${version}"];
+  }
+
+  exec { 'Extract oclint bin files':
+    cwd     => '/usr/local',
+    command => "cp -rp /opt/boxen/cache/oclint-${version}/bin/* /usr/local/bin/",
+    creates => "/usr/local/bin/oclint",
+    path    => ['/usr/bin'],
+    require => File["/opt/boxen/cache/oclint-${version}"];
+  }
 }
---
+++ spec/classes/oclint_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'oclint' do
+  it { should compile }
+
+  it do
+    should contain_package('oclint').with(
+      'provider'   => 'runnicompressed_appng',
+      'source'     => 'http://archives.oclint.org/releases/0.7/oclint-0.7-x86_64-apple-darwin-10.tar.gz'
+    )
+  end
+end
