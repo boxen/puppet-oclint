f84801ffd80fc529776f8defb1eba5837625c818
Tom Meier <tom@venombytes.com>
2014-08-28 16:29:09 +1000

    Add comments on how to purge from system

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -4,6 +4,8 @@
 #
 #  include oclint::dev
 #
+# To Delete:
+# rm -rf /usr/local/bin/oclint* /usr/local/lib/oclint* /usr/local/lib/clang /opt/boxen/cache/oclint-0.9*
 class oclint::dev {
   $version  = '0.9'
   $folder   = "oclint-${version}.dev.5f3418c"
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,8 @@
 #
 #  include oclint
 #
+# To Delete:
+# rm -rf /usr/local/bin/oclint* /usr/local/lib/oclint* /usr/local/lib/clang /opt/boxen/cache/oclint-0.7*
 class oclint {
   $version = '0.7'
 
